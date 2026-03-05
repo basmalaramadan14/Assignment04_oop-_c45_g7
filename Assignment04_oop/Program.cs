41aa05de174a8f2fbc9287b49054e85d8a3d0d65
Basmala Ramadan <basmalaramadan51@gmail.com>
2026-03-06 01:12:24 +0200

    Q01 Part01

--- Assignment04_oop/Program.cs
+++ Assignment04_oop/Program.cs
@@ -2,7 +2,20 @@
 namespace Assignment04_oop
 
 {
-   
+    #region Q01 Part 01 
+
+    //--Static Binding when the method call is resolved before the program runs
+    //usually occurs with non-virtual methods and method overloadin
+    //--happens at compile time
+
+    //--Dynamic Binding:when the method call is resolved while the program is running,
+    //usually with virtual methods and method overriding
+    //--happens at runtime
+
+
+    #endregion
+
+    
     internal class Program
     {
         static void Main(string[] args)
